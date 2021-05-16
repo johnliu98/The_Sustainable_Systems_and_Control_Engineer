97e2b3b607b639c7ffd3398a38a71e5e73edae1c
johnliu98 <68383339+johnliu98@users.noreply.github.com>
2021-05-16 22:27:24 +0200

    Modeling a fishing scenario h)

--- Seminar 4/modeling_a_fishing_scenario.m
+++ Seminar 4/modeling_a_fishing_scenario.m
@@ -1,23 +1,27 @@
 %{
-   1.2 Modeling a fishing scenario 
+    1.2 Modeling a fishing scenario 
+
+    DEPENDENCIES:
+    - matlab2tikz package (see "http://www.mathworks.com/matlabcentral/fileexchange/22022-matlab2tikz-matlab2tikz")
+      in "MATLAB\R2021a\toolbox\matlab\plottools"-folder
 %}
 
 close all;
 clear all;
 clc;
 
+%% Add path to matlab2tikz package
 paths = split(path,';');
 expr = 'plottools$';
 for i=1:numel(paths)
     if ~isempty(regexp(paths{i},expr,'once'))
         matlab2tikz_path = paths{i};
+        break
     end
 end
 
 if exist('matlab2tikz_path','var')
-    matlab2tikz_path = strcat(matlab2tikz_path,...
-        '\matlab2tikz\src');
-    
+    matlab2tikz_path = strcat(matlab2tikz_path,'\matlab2tikz\src');
     addpath(matlab2tikz_path,'-end')
 end
 
@@ -71,21 +75,25 @@ fe = @(x) fe_max * 6*x./(100+6*x);
 
 %% c) Dynamics of Fish Population
 x_max = 2000;
-fx = @(x,y) fr(100*x./x_max) - y.*fe(100*x./x_max);
+fx = @(x,y) (fr(100*x./x_max) - y.*fe(100*x./x_max));
 
 %% d) Equilibrium points and region of attraction
 %{
 syms x
 
-N = 41;
+N = 40;
 y_max = 40;
-equi = zeros(4,N);
-y = linspace(0.01,y_max,N);
+equi = zeros(4,N+1);
+y = linspace(0,y_max,N+1);
 
-ubs = zeros(1,N);
-lbs = zeros(1,N);
-for i=1:N
-    equi(:,i) = double(solve(fx(x,y(i))));
+ubs = zeros(1,N+1);
+lbs = zeros(1,N+1);
+for i=1:N+1
+    solv = double(solve(fx(x,y(i))));
+    if numel(solv) < 4
+        solv = [zeros(4-numel(solv),1); solv];
+    end
+    equi(:,i) = solv;
     
     df = diff(sym(@(x)fx(x,y(i))));
     bounds = double(solve(df));
@@ -95,32 +103,32 @@ for i=1:N
 end
 eq = equi;
 equi = real(equi);
-equi(2,:) = [equi(2,1:17) 785 equi(3,19:end)];
+equi(2,:) = [equi(2,1:18) 830 equi(3,20:end)];
 equi(3,:) = [];
 
 figure, hold on
-plot(1:N,equi(1,:),'Color',[1, 0, 0])
-plot(1:25,equi(2,1:25),'Color',[0.9290, 0.6940, 0.1250])
-plot(1:25,equi(3,1:25),'Color',[0, 0.5, 0])
+plot(0:N,equi(1,:),'Color',[1, 0, 0],'LineWidth',2)
+plot(0:24,equi(2,1:25),'Color',[0.9290, 0.6940, 0.1250],'LineWidth',2)
+plot(0:24,equi(3,1:25),'Color',[0, 0.5, 0],'LineWidth',2)
 ylim([0 x_max])
 xlim([0 y_max])
 grid on
 
-x = 1:N;
+x = 0:N;
 xx = [x fliplr(x)];
 yy1 = [equi(1,:) fliplr(equi(2,:))];
 fill(xx,yy1,'r','FaceAlpha',0.1,'EdgeColor','none')
 
-x = 1:25;
+x = 0:24;
 xx = [x fliplr(x)];
 yy2 = [equi(2,1:25) fliplr(equi(3,1:25))];
 yy3 = [equi(3,1:25) fliplr(2000*ones(1,25))];
 fill(xx,yy2,'g','FaceAlpha',0.1,'EdgeColor','none')
 fill(xx,yy3,'g','FaceAlpha',0.1,'EdgeColor','none')
 
-x = 25:N;
+x = 24:N;
 xx = [x fliplr(x)];
-yy4 = [equi(3,x) 2000*ones(size(equi(x)))];
+yy4 = [equi(3,25:end) 2000*ones(size(equi(3,25:end)))];
 fill(xx,yy4,'r','FaceAlpha',0.1,'EdgeColor','none')
 
 xlabel('Fishing Boats')
@@ -136,19 +144,13 @@ end
 ky = [0.1 0.5 1];
 c = [20 22 24];
 [ky,c] = meshgrid(ky,c);
-color = [0, 0.4470, 0.7410;
-         0.8500, 0.3250, 0.0980;
-         0.9290, 0.6940, 0.1250;
-         0.4940, 0.1840, 0.5560;
-         0.4660, 0.6740, 0.1880;
-         0.3010, 0.7450, 0.9330];
 
- %{
+%{
 fig = figure;
 for i=1:numel(ky)
     
     fy = @(x,y) ky(i)*y.*(fe(100*x./x_max)-c(i));
-    [X1,X2] = meshgrid(linspace(0,2000,10),linspace(0,200,10));
+    [X1,X2] = meshgrid(linspace(0,2000,100),linspace(0,80,100));
     F1 = fx(X1,X2);
     F2 = fy(X1,X2);
     
@@ -167,12 +169,48 @@ for i=1:numel(ky)
     xticks([0 1000 2000])
     xticklabels({'0','1000','2000'})
     
-    axis([0 2000 0 200])
+    axis([0 2000 0 80])
     grid on
 end
+fig.Position(3:4) = 0.975*fig.Position(3:4);
 
-
-if exist('matlab2tikz_path','var')
-    matlab2tikz('figures\dynamic_ships.tex','showInfo', false);
-end
+fname = 'figures/';                             % figure directory path
+filename = 'dynamic_ships';                     % file name
+saveas(gca, fullfile(fname, filename), 'epsc')  % 'epsc' for colored plots
 %}
+
+%% Subsidies for struggling fishing boats
+%{
+fig = figure;
+for i=1:numel(ky)
+    
+    fy = @(x,y) ky(i)*y.*max(fe(100*x./x_max)-c(i),-2);
+    [X1,X2] = meshgrid(linspace(0,2000,100),linspace(0,80,100));
+    F1 = fx(X1,X2);
+    F2 = fy(X1,X2);
+    
+    subplot(3,3,i)
+    streamslice(X1,X2,F1,F2,0.2,'noarrows');
+    
+    if i==8
+        xlabel('Fish')
+    end
+    if i==4
+        ylabel('Fishing Boats')
+    end
+    
+    title(sprintf('ky=%.1f, c=%.0f', [ky(i),c(i)]))
+    
+    xticks([0 1000 2000])
+    xticklabels({'0','1000','2000'})
+    
+    axis([0 2000 0 80])
+    grid on
+end
+fig.Position(3:4) = 0.9*fig.Position(3:4);
+
+fname = 'figures/';                             % figure directory path
+filename = 'subsidies';                         % file name
+saveas(gca, fullfile(fname, filename), 'epsc')  % 'epsc' for colored plots
+%}
+
